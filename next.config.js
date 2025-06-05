060452bf802afd2abfc2abbabc0a2cef6705a5aa
Andrey Rogovsky <andrey.rogovsky@1win.pro>
2025-06-05 03:48:08 +0300

    total

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
   },
   productionBrowserSourceMaps: false,
+  experimental: {
+    modern: true,
+  },
+  browserslist: [
+    "last 2 Chrome versions",
+    "last 2 Firefox versions",
+    "last 2 Edge versions",
+    "last 2 Safari versions",
+  ],
 };
 
 module.exports = nextConfig;
